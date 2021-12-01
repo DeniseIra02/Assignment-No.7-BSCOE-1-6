a692a8bac296334c29874c9745297c8ac3b6d172
DeniseIra02 <irajubilo@gmail.com>
2021-12-01 20:10:13 +0800

    Set three variables and created a for loop statement for number of words in the sentence.

--- Prog1.py
+++ Prog1.py
@@ -2,3 +2,13 @@ print("Hello there!")
 
 yourSntnc = input("Please Enter your Sentence : \n> ")
 print("------------------------------------------")
+
+num_space = 0
+num_vowels = 0
+num_consonant = 0
+
+for ch_word in yourSntnc:
+    if ch_word == ' ':
+        num_space = num_space + 1
+        
+        num_words = num_space + 1

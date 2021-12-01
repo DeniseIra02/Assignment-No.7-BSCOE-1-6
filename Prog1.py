a97c19196c691ff04e67536e3912e406869fd557
DeniseIra02 <irajubilo@gmail.com>
2021-12-01 20:28:29 +0800

    Added another for loop statement for the number of vowels and consonant using if else statement.

--- Prog1.py
+++ Prog1.py
@@ -7,8 +7,23 @@ num_space = 0
 num_vowels = 0
 num_consonant = 0
 
+#number of words
 for ch_word in yourSntnc:
     if ch_word == ' ':
         num_space = num_space + 1
         
         num_words = num_space + 1
+
+#number of vowels and consonants
+for letter in range(0, len(yourSntnc)):
+         
+    input_letters = yourSntnc[letter]
+ 
+    if ( (input_letters >= 'a' and input_letters <= 'z') or (input_letters >= 'A' and input_letters <= 'Z') ):
+   
+        input_letters = input_letters.lower()
+ 
+        if (input_letters == 'a' or input_letters == 'e' or input_letters == 'i' or input_letters == 'o' or input_letters == 'u'):
+                num_vowels += 1
+        else:
+                num_consonant += 1

d0926ee54889374e447ac38621bfc1b52eb0530e
DeniseIra02 <irajubilo@gmail.com>
2021-12-02 20:10:26 +0800

    Add import statements that can be used in the program. Used print statements for the introduction in display.

---
+++ Prog2.py
@@ -0,0 +1,7 @@
+import re
+
+print("----------------------------------------")
+print("Hey! Please Sign Up to Access")
+print("----------------------------------------")
+print("Note!: \n The password is valid if all criteria are met: \n a. Greater than 15 letters \n b. Have at least one capital letter \n c. Have at least one number \n d. Have at least one special character (!@#$%^&*()_+<>)")
+print("----------------------------------------")
